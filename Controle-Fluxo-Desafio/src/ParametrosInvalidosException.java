662fd42acd9f58d7454211252f97b12f8f3f5052
Allan Richard <allanrichard1345@gmail.com>
2024-07-14 22:59:32 -0300

    Projeto Atualizado

---
+++ Controle-Fluxo-Desafio/src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+// ParametrosInvalidosException.java
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
